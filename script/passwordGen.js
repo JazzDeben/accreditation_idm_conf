2360484d61275857bd0e789f078f3c9ea37d8a43
Gery Ducatel <gery.charleston@gmail.com>
2023-01-12 22:53:31 +0000

    sync for mysql, and ad

---
+++ script/passwordGen.js
@@ -0,0 +1,33 @@
+(function (){
+    let pwd = generatePassword();
+    return { password : pwd };
+})();
+
+function generatePassword() {
+    var pwd = '';
+    var capital = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+    var special = '!*()';
+    var numbers = '0123456789';
+              
+    for (let x = 0; x < 16; x++) {
+        switch (true) {
+            case ((x >= 0) && ( x < 4)):
+                var loc = Math.floor(Math.random() * (capital.length -1) +1 );
+                pwd += capital.charAt(loc);
+                break;
+            case ((x >= 4) && ( x < 8)):
+                var loc = Math.floor(Math.random() * (lowercase.length -1) +1 );
+                pwd += lowercase.charAt(loc);
+                break;
+            case ((x >= 8) && ( x < 12)):
+                var loc = Math.floor(Math.random() * (special.length -1) +1 );
+                pwd += special.charAt(loc);
+                break;
+            case ((x >= 12) && ( x < 16)):
+                var loc = Math.floor(Math.random() * (numbers.length -1) +1 );
+                pwd += numbers.charAt(loc);
+        }
+    }
+    return pwd;
+}
